cf622d02c4beafc22c5518a0b18801af854dc010
Daniel Muñoz Martín <info@danimm.com>
2021-09-28 11:22:26 +0200

    user Module updated

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserService } from '../service/user.service';
+import { CreateUserDto, UpdateUserDto } from '../../../dtos/user.dtos';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  getUsers() {
+    return {
+      data: this.userService.findAll(),
+    };
+  }
+
+  @Get()
+  getUser(@Param('userId') userId: string) {
+    return this.userService.findOne(userId);
+  }
+
+  @Post()
+  createUser(@Body() payload: CreateUserDto) {
+    return this.userService.create(payload);
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() payload: UpdateUserDto) {
+    return this.userService.update(id, payload);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.delete(id);
+  }
+}
--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, HttpStatus } from '@nestjs/common';
+import { User } from 'src/entitites/user.entity';
+import { CreateUserDto, UpdateUserDto } from '../../../dtos/user.dtos';
+import { nanoid } from 'nanoid';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [];
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findOne(id: string) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException({
+        error: `User #${id} not found`,
+        status: HttpStatus.NOT_FOUND,
+      });
+    }
+    return user;
+  }
+
+  create(payload: CreateUserDto) {
+    const newUser: User = { id: nanoid(), ...payload };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: string, payload: UpdateUserDto) {
+    const index = this.users.findIndex((user) => user.id === id);
+
+    if (index < 0) {
+      throw new NotFoundException({
+        error: `User #${id} not found`,
+        status: HttpStatus.NOT_FOUND,
+      });
+    }
+
+    const user = this.findOne(id);
+    this.users[index] = {
+      ...user,
+      ...payload,
+    };
+    return { message: 'Data correctly updated' };
+  }
+
+  delete(id: string) {
+    const selectedUser = this.users.findIndex((user) => user.id === id);
+
+    if (selectedUser < 0) {
+      throw new NotFoundException({
+        error: `User #${id} not found`,
+        status: HttpStatus.NOT_FOUND,
+      });
+    }
+
+    this.users.splice(selectedUser, 1);
+    return { message: 'User deleted correctly' };
+  }
+}
